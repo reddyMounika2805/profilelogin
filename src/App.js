050cab6af4627c5be3333872a8f53ccdb0ba507f
mounika <reddymounika2805@gmail.com>
2023-05-06 20:16:32 +0530

    profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
+export default function UserInfoForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>First Name</label>
+        <input
+          type="text"
+          {...register("firstName", { required: true, minLength: 5 })}
+        />
+        {errors.firstName && <span>This field is required and must have at least 5 characters</span>}
+      </div>
+      <div>
+        <label>Last Name</label>
+        <input type="text" {...register("lastName")} />
+      </div>
+      <div>
+        <label>Email Address</label>
+        <input type="email" {...register("email", { required: true })} />
+        {errors.email && <span>This field is required</span>}
+      </div>
+      <div>
+        <label>Phone Number</label>
+        <input
+          type="text"
+          {...register("phone", {
+            required: true,
+            pattern: {
+              value: /^[0-9]{10}$/,
+              message: "Invalid phone number",
+            },
+          })}
+        />
+        {errors.phone && <span>{errors.phone.message}</span>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
-
-export default App;
